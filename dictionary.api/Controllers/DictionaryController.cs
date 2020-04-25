d4d5995ddb6bc21cbe814e4df5520703877f6dc3
Piotr Rybka <piotr.rybka82@gmail.com>
2020-04-26 01:04:43 +0200

    pierwsza wersja modulu web api - testy routingu dictionary/browser/find?form w postmanie

---
+++ dictionary.api/Controllers/DictionaryController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dictionary.Core.Models;
+using Dictionary.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Dictionary.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DictionaryController : ControllerBase
+    {
+        private readonly IDictionary _dictionary;       
+        private readonly ILogger<DictionaryController> _logger;
+
+        public DictionaryController(
+            ILogger<DictionaryController> logger,
+            IDictionary dictionary)
+        {
+            _logger = logger;
+            _dictionary = dictionary;
+        }
+
+        [HttpGet("welcome")]
+        public string Welcome()
+        {
+            return "Here is the New Grammatical Dictionary of Polish";
+        }
+
+        //dictionary/find?form=
+        [HttpGet("browser/find")]
+        //public async Task<IEnumerable<object>> Find(string form = "")
+        public IEnumerable<Entry> Find(string form = "")
+        {
+            //return new[] { form };
+
+            if (form != "")
+            {
+                //return await _dictionary.GetEntriesAsync(form);
+                return _dictionary.GetEntries(form);
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+
+
+
+    }
+}
--- dictionary.api/Startup.cs
+++ dictionary.api/Startup.cs
@@ -2,16 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Http.Routing;
+using Dictionary.Core;
+using Dictionary.Core.Services;
+using Dictionary.Data;
+using Dictionary.Service;
+using Dictionary.Service.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
-namespace dictionary.api
+namespace Dictionary.Api
 {
     public class Startup
     {
@@ -25,6 +33,22 @@ namespace dictionary.api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //MongoDb
+            services.Configure<DatabaseSettings>(Configuration.GetSection(nameof(DatabaseSettings)));
+
+            services.AddSingleton<IDatabaseSettings>(dbSettings => dbSettings.GetRequiredService<IOptions<DatabaseSettings>>().Value);
+
+
+            //unit of work
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+            //core
+            services.AddTransient<IDictionary, DictionaryService>();
+            services.AddTransient<IGenerator, GeneratorService>();
+            services.AddTransient<ILemmatizer, LemmatizerService>();
+
+
             services
                 .AddControllers()
                 .SetCompatibilityVersion(CompatibilityVersion.Latest); //zapewnia kompatybilność ze wskazaną wersją frameworka
@@ -50,7 +74,10 @@ namespace dictionary.api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Dictionary}/{action=Find}/{form}");
+                //endpoints.MapControllers();
             });
         }
     }
